1467bf43e574119364c263cd01a7738c76f83140
Ramirogal <110607678+Ramirogal@users.noreply.github.com>
2025-07-31 14:22:28 -0300

    Agrego archivo config para uso de datos local y modificico hook

--- bazar-universal/src/hooks/useFetchProducts.jsx
+++ bazar-universal/src/hooks/useFetchProducts.jsx
@@ -1,22 +1,42 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
+import { USE_LOCAL_DATA } from '../config'
+import Products from '../data/products.json'
 
 export function useFetchById (id) {
   const [product, setProduct] = useState(null)
-  
+  const [ loading, setLoading ] = useState(false)
+  const [ error, setError ] = useState(null)
+
   useEffect(() => {
     if (!id) {
       setProduct(null)
       return
     }
     const fetchById = async () => {
-      const response = await fetch(`http://localhost:1234/items/${id}`)
-      const data = await response.json()
+      let data
+      try {
+          if (USE_LOCAL_DATA) {
+            const productList = Products.products
+            data = productList.filter(p => Number(p.id) === id)
+            if (data.length === 0) {
+              throw new Error ('Product not found')
+            }
+          } else {
+            const response = await fetch(`http://localhost:1234/items/${id}`)
+            data = await response.json()
+          }
+        } catch (e) {
+          setProduct([])
+          setError(e.message)
+        } finally {
+          setLoading(false)
+        }
       setProduct(data)
     }
     fetchById()
   }, [id])
 
-  return { product }
+  return { product, loading, error }
 }
 
 export function useFetchProducts (query) {
@@ -32,16 +52,26 @@ export function useFetchProducts (query) {
       setLoading(true)
       setError(null)
       try {
-        const response = await fetch(`http://localhost:1234/items?q=${query}`)
-        if (!response.ok) throw new Error(`Error: ${response.status}`)
-        const data = await response.json()
+        let data
+        if (USE_LOCAL_DATA) {
+          const listProducts = Products.products
+          data = listProducts.filter(p => (
+            p.title.toLowerCase().includes(query.toLowerCase()) || 
+            p.category.toLowerCase().includes(query.toLowerCase())
+          ))
+        } else {
+          const response = await fetch(`http://localhost:1234/items?q=${query}`)
+          if (!response.ok) {
+            throw new Error(`Error: ${response.status}`)
+          }  
+          data = await response.json()
+        }
+
         setProducts(data)
-      }
-      catch (e) {
+      } catch (e) {
         setProducts([])
         setError(e.message)
-      }
-      finally {
+      } finally {
         setLoading(false)
       }
     }
